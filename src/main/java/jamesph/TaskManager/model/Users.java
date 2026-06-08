669b27df675916617cf4cd0d7d650c07ddb15b5f
unknown <vergeljamesviray@gmail.com>
2026-06-08 11:10:25 +0800

    feat: Created entity for USER model. Modified the Task Entitity added additional columns. Added TODO comment on BaseModel for status column.

--- src/main/java/jamesph/TaskManager/model/Users.java
+++ src/main/java/jamesph/TaskManager/model/Users.java
@@ -1,30 +1,35 @@
 package jamesph.TaskManager.model;
 
-
+import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jamesph.TaskManager.model.common.BaseModel;
 import lombok.Getter;
 import lombok.Setter;
 
+//TODO Find a way to change the table name from users to user
 @Entity
-@Table(name = "`user`")
+@Table(name = "users")
 @Getter
 @Setter
-public class User extends BaseModel {
+public class Users extends BaseModel {
 
   @Id
-  @Column(name = "`uuid`", insertable = true, updatable = false, nullable = false)
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID uuid;
 
   @Column(name = "first_name", length = 50, nullable = false)
   private String firstName;
 
-  @Column(name = "middle_name", length = 50, nullable = false)
+  @Column(name = "middle_name", length = 50)
   private String middleName;
 
   @Column(name = "last_name", length = 50, nullable = false)
@@ -35,5 +40,8 @@ public class User extends BaseModel {
 
   @Column(name = "phone_number")
   private Integer phoneNumber;
+
+  @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Task> tasks;
 
 }
